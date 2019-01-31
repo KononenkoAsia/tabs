d630f3eece5e9a2877c51bec63d9de27875336fd
KononenkoAsya <ananaskononenko@gmail.com>
2019-01-31 17:47:51 +0200

    Display relevant content on clicking on the tab

--- src/components/content.js
+++ src/components/content.js
@@ -2,12 +2,16 @@ import React from 'react';
 
 const Content = (props) => {
 
+	return props.content.filter((content) => {return content.id===props.id}).map((value) => {
 		return(
 			<div className="content" key={value.id}>
 				<p>
+					{value.text}
 				</p>
 			</div>
 		);		
+	});	
+		
 }
 
 export default Content;
